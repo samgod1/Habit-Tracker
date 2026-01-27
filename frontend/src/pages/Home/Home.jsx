c6f71aaa2072210be54553bcfc6c8a4e6cb33399
SamGod69 <sampannatamrakar@gmail.com>
2026-01-27 16:28:35 +0545

    Somewhat added an animated svg in Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,9 +2,12 @@ import { useNavigate } from "react-router-dom";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useContext } from "react";
+import { ScrollTrigger } from "gsap/all";
 
 import "./Home.css";
 import { PageTransitionContext } from "../../components/PageTransition/PageTransition";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Home = () => {
 	const { handlePageChange } = useContext(PageTransitionContext);
@@ -20,6 +23,24 @@ const Home = () => {
 			opacity: 0,
 			yPercent: 50,
 			duration: 0.5,
+		});
+
+		//svg-path animation
+		const path = document.getElementById("stroke-path");
+		const pathLength = path.getTotalLength();
+
+		path.style.strokeDasharray = pathLength;
+		path.style.strokeDashoffset = pathLength;
+
+		gsap.to(path, {
+			strokeDashoffset: 0,
+			scrollTrigger: {
+				trigger: ".spotlight-section",
+				start: "top top",
+				end: "bottom bottom",
+				scrub: true,
+				markers: true,
+			},
 		});
 	}, []);
 
@@ -41,15 +62,25 @@ const Home = () => {
 					</button>
 				</div>
 			</section>
-			<svg viewBox="0 0 200 200" width="200" height="200">
-				<path
-					id="myPath"
-					d="M 10 80 Q 95 10 180 80"
-					stroke="#000"
-					stroke-width="3"
-					fill="none"
-				/>
-			</svg>
+			<section className="spotlight-section">
+				<div id="svg-path">
+					<svg
+						width="1051"
+						height="2832"
+						viewBox="0 0 1051 2832"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							id="stroke-path"
+							d="M588.89 80.015C588.89 80.015 167.332 161.997 167.332 494.565C167.332 827.134 940.229 1144.96 968.844 1497.27C1021.85 2149.86 80 2071.66 80 1740.3C80 1408.94 683.5 1452.01 683.5 2751.01"
+							stroke="black"
+							stroke-width="160"
+							stroke-linecap="round"
+						/>
+					</svg>
+				</div>
+			</section>
 		</main>
 	);
 };
